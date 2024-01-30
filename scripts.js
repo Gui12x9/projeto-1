a979cc48b59f14c15c52108f56ddb0354ebed997
Gui12x9 <guilhermejesus568@gmail.com>
2024-01-30 20:03:19 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+function troca(imagem) {
+    let imgtelefone = document.querySelector(".telefone")
+    imgtelefone.src = imagem
+}
